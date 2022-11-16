6bf7d3717153eaf9f72323e460168b43116af943
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-11-16 10:46:15 -0600

    background gradient added

--- js/character-sheet.js
+++ js/character-sheet.js
@@ -14,30 +14,30 @@
         exp: null,
     }
 
-    function renderChar(char) {
-        let html = '<div className="name-div">' +
-            '           <strong>Character Name: </strong>' +
-            '           <p className="name">' + char.name + '</p>' +
-            '       </div>';
-        html += '<div className="char-div">\n' +
-            '        <strong>Race: </strong>' +
-            '        <p className="race">' + char.race + '</p>\n' +
-            '        <strong>Background: </strong>' +
-            '        <p className="background">' + char.background + '</p>\n' +
-            '        <strong>Alignment: </strong>' +
-            '        <p className="alignment">' + char.alignment + '</p>\n' +
-            '        <strong>Exp: </strong>' +
-            '        <p className="exp-points">' + char.exp + '</p>\n' +
-            '    </div>';
-        html += '<div className="class-div">\n' +
-            '        <strong>Class: </strong>' +
-            '        <p className="class">' + char.class + '</p>\n' +
-            '        <strong>Subclass: </strong>' +
-            '        <p className="subclass">' + char.subclass + '</p>\n' +
-            '    </div>';
-
-        return html;
-    }
+    // function renderChar(char) {
+    //     let html = '<div className="name-div">' +
+    //         '           <strong>Character Name: </strong>' +
+    //         '           <p className="name">' + char.name + '</p>' +
+    //         '       </div>';
+    //     html += '<div className="char-div">\n' +
+    //         '        <strong>Race: </strong>' +
+    //         '        <p className="race">' + char.race + '</p>\n' +
+    //         '        <strong>Background: </strong>' +
+    //         '        <p className="background">' + char.background + '</p>\n' +
+    //         '        <strong>Alignment: </strong>' +
+    //         '        <p className="alignment">' + char.alignment + '</p>\n' +
+    //         '        <strong>Exp: </strong>' +
+    //         '        <p className="exp-points">' + char.exp + '</p>\n' +
+    //         '    </div>';
+    //     html += '<div className="class-div">\n' +
+    //         '        <strong>Class: </strong>' +
+    //         '        <p className="class">' + char.class + '</p>\n' +
+    //         '        <strong>Subclass: </strong>' +
+    //         '        <p className="subclass">' + char.subclass + '</p>\n' +
+    //         '    </div>';
+    //
+    //     return html;
+    // }
 
     // function renderCoffees(coffees) {
     //     let html = '';
